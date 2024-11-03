ece10ca49d7c8a903d52dfa4d57cf3e740eaa433
Raival-e <se.10.sec.ac.180@gmail.com>
2024-11-03 20:45:11 +0400

    chore: bump version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.raival.compose.file.explorer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     dependenciesInfo {
