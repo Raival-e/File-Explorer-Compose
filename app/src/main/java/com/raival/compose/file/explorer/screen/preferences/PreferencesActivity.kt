956b11321b91657affb070802999ffb936c596df
Raival-e <se.10.sec.ac.180@gmail.com>
2024-11-14 16:45:51 +0400

    fix: remove unused imports

--- app/src/main/java/com/raival/compose/file/explorer/screen/preferences/PreferencesActivity.kt
+++ app/src/main/java/com/raival/compose/file/explorer/screen/preferences/PreferencesActivity.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
