ae83e5f85493ed518ebdc299b0614519072fa2ec
Raival-e <se.10.sec.ac.180@gmail.com>
2025-01-28 12:17:14 +0400

    fix: show APK and PDF icons

--- app/src/main/java/com/raival/compose/file/explorer/screen/main/tab/files/modal/DocumentHolder.kt
+++ app/src/main/java/com/raival/compose/file/explorer/screen/main/tab/files/modal/DocumentHolder.kt
@@ -96,6 +96,8 @@ data class DocumentHolder(val documentFile: DocumentFile) : ContentHolder() {
 
     val isVideo by lazy { isFile && videoFileType.contains(fileExtension) }
 
+    val isPdf by lazy { isFile && fileExtension == pdfFileType }
+
     val basePath by lazy { documentFile.getBasePath(globalClass) }
 
     val storageId by lazy { documentFile.getStorageId(globalClass) }
