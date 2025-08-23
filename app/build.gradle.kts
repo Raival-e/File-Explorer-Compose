17c7b9a19d939c37ea3cd3d004e1715f058dc813
Raival-e <s10.fir.acc190@gmail.com>
2025-08-23 14:37:29 +0400

    Update app version to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.raival.compose.file.explorer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.3.1"
+        versionCode = 10
+        versionName = "1.3.2"
     }
 
     dependenciesInfo {
